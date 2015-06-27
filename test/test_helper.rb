e60940b6f689d34ffa2c4b652534d4ee2f3beba4
winsday <sunqinshan@hotmail.com>
2015-06-27 16:03:52 +0000

    finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,4 +8,27 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
+  #登入测试用户
+  def log_in_as(user, options = {})
+    password = options[:password] || '123456'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: {email: user.email,
+                                  password: password,
+                                  remember_me: remember_me}
+    else
+      session[:user_id] = user.id
+    end
+  end
+  
+  private
+  
+  #判断是否为集成测试
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
